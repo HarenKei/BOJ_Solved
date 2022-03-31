4b625c3dd6038762abaf8ada0b6cdf6e98c7e71a
Seungmin Jeong <seungmin1266@gmail.com>
2022-03-31 18:06:01 +0900

    Solving 11944 / cpp

---
+++ BOJ_Cpp/11944.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string sNum;
+    int mNum;
+
+    cin >> sNum >> mNum;
+
+    
+        for(int j = 0; j < sNum.length(); j++){
+            cout << sNum[j];
+        }
+    
+    return 0;
+}
