b2801d6534f40ed5a380da74d086f9d6fb881f45
Seungmin Jeong <seungmin1266@gmail.com>
2022-04-07 11:41:39 +0900

    wip 1935

--- BOJ_DS/1935.cpp
+++ BOJ_DS/1935.cpp
@@ -8,6 +8,54 @@ int main(){
     cout.tie(NULL);
 
     int n;
+    string str;
+    vector<double> vec;
+    stack<double> st;
+
     cin >> n;
+    cin >> str;
     
+
+    for(int i = 0; i < n; i++){
+        double num;
+        cin >> num;
+        //vec.push_back(num);
+    
+
+    for(int j = 0; j < str.length(); j++){
+        if (str[j] == '+' || str[j] == '-' || str[j] == '*' || str[j] == '/'){
+            double b = st.top();
+            st.pop();
+            double a = st.top();
+            st.pop();
+
+            switch (str[j]){
+                case '+':
+                    st.push(a + b);
+                    break;
+                case '-':
+                    st.push(a - b);
+                    break;
+                case '*':
+                    st.push(a * b);
+                    break;
+                case '/':
+                    st.push(a / b);
+                    break;
+            }
+        } else{
+            st.push(str[j]=num);
+        }
+    }
+    }
+    double result = st.top();
+
+        cout << fixed;
+
+        cout.precision(2);
+
+        cout << result << "\n";
+
+        return 0;
+
 }
