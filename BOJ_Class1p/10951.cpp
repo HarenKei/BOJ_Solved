0f0f40ec09475cf146c56ec933882e9e1be7b1a9
Seungmin Jeong <seungmin1266@gmail.com>
2022-04-09 16:30:25 +0900

    Solved 10951 /cpp

---
+++ BOJ_Class1p/10951.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while(1){
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
