90b3e3d81e1a6fb2e6e2b9c8f11b25b2a7e7288c
Seungmin Jeong <seungmin1266@gmail.com>
2022-07-01 18:05:12 +0900

    solved 1018 / cpp

--- BOJ_Class2p/1018.cpp
+++ BOJ_Class2p/1018.cpp
@@ -1,36 +1,63 @@
 #include <bits/stdc++.h>
+#include <string.h>
 
 using namespace std;
 
+vector<string> myChess;
+
+string vec_wb[8] = {
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW"
+    };
+
+string vec_bw[8] = {
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB"
+    };
+
+int chkWhite(int x, int y){
+    int cnt = 0;
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            if(myChess[x+i][y+j] != vec_wb[i][j]) 
+                cnt++;
+        }   
+    }
+    return cnt;
+}
+
+int chkBlack(int x, int y){
+    int cnt = 0;
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            if(myChess[x+i][y+j] != vec_bw[i][j])
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
 int main(){
-    ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false); 
     cin.tie(NULL);
     cout.tie(NULL);
 
-    vector<string> vec_wb = {
-                                "WBWBWBWB",
-                                "BWBWBWBW",
-                                "WBWBWBWB",
-                                "BWBWBWBW",
-                                "WBWBWBWB",
-                                "BWBWBWBW",
-                                "WBWBWBWB",
-                                "BWBWBWBW",
-    };
-
-    vector<string> vec_bw = {
-                                "BWBWBWBW",
-                                "WBWBWBWB",
-                                "BWBWBWBW",
-                                "WBWBWBWB",
-                                "BWBWBWBW",
-                                "WBWBWBWB",
-                                "BWBWBWBW",
-                                "WBWBWBWB",                               
-    };
-    
     int n, m;
-    vector<string> myChess;
+    int temp = 0;
+    int minVal = 999;
+    
     cin >> n >> m;
 
     for(int i = 0; i < n; i++){
@@ -38,45 +65,17 @@ int main(){
         cin >> str;
         myChess.push_back(str);
     }
-    
-    int min = 65;
-    
-    for(int i = 0; i < n - 7; i++){
-        for(int j = 0; j < m - 7; j++){
-            int tempCnt = 0;
 
-            if(myChess[i][j] == 'W'){
-                for(int q = 0; q < 8; q++){
-                    for(int z = 0; z < 8; z++){
-                        for(int k = i; k < i + 7; k++){
-                            for(int l = j; l < j + 7; j++){
-                                if(myChess[k][l] != vec_wb[i+k][j+l]){
-                                    tempCnt++;
-                                    if(min > tempCnt) min = tempCnt;
-                                }
-                            }
-                        }
-                    }
-                }
-            } else{
-                for(int q = 0; q < 8; q++){
-                    for(int z = 0; z < 8; z++){
-                        for(int k = i; k < i + 7; k++){
-                            for(int l = j; l < j + 7; j++){
-                                if(myChess[k][l] != vec_bw[i+k][j+l]){
-                                    tempCnt++;
-                                    if(min > tempCnt) min = tempCnt;
-                                }
-                            }
-                        }
-                    }
-                }
-
+    for(int x = 0; x <= n - 8; x++){
+        for(int y = 0; y <= m - 8; y++){ //자를 수 있는 시작점의 개수만큼 반복.
+            temp = min(chkBlack(x, y), chkWhite(x, y));
+            if(temp < minVal){
+                minVal = temp;
             }
         }
     }
-
-    cout << min << '\n';
+    
+    cout << minVal << '\n';
 
     return 0;
 }
