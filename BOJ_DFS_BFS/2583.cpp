59a83b49a49c78777fd2d6b74861c4351dba0ce3
Seungmin Jeong <seungmin1266@gmail.com>
2022-07-30 17:03:39 +0900

    Solving 2583

---
+++ BOJ_DFS_BFS/2583.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define MAX 101
+
+using namespace std;
+
+int m, n, k; //m -> y, n -> x, k는 직사각형의 개수ㅇ
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
