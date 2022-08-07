0bd3f10d32bb0d7916c1e71cb364bd1522b3a642
Seungmin Jeong <seungmin1266@gmail.com>
2022-08-07 23:53:35 +0900

    Solved 9655 / cpp

---
+++ DP/9655.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    if(n % 2 == 0){
+        cout << "CY";
+    } else{
+        cout << "SK";
+    }
+
+    return 0;
+}
+    
