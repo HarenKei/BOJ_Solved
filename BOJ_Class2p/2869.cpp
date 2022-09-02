ffa1a4ba9255441d5c069afecf8b71ed31dd206a
Seungmin Jeong <seungmin1266@gmail.com>
2022-09-02 14:58:01 +0900

    Solved 2869 / cpp

--- BOJ_Class2p/2869.cpp
+++ BOJ_Class2p/2869.cpp
@@ -2,26 +2,18 @@
 
 using namespace std;
 
+int a, b, v;
+int ans = 1;
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int a, b, v;
-    int count = 1;
     cin >> a >> b >> v;
-    
-   while(true){
-       int c = (a - b);
-       c += c;
-       count++;
+    ans += ((v - a) % (a - b) == 0) ? (v - a) / (a - b) : ((v - a) / (a - b)) + 1;
 
-       if(c >= v){
-           break;
-       }
-   }
-
-    cout << count << endl;
+    cout << ans << endl;
 
     return 0;
 }
