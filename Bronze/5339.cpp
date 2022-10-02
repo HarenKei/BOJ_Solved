98bfb828bcd29dd41e36d4c0380ef43bdce2b2b4
Seungmin Jeong <seungmin1266@gmail.com>
2022-10-02 23:28:28 +0900

    Solved 5399 / cpp / Bronze V

---
+++ Bronze/5339.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, ans, tmpSum;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cout << "     /~\\\n";
+    cout << "    ( oo|\n";
+    cout << "    _\\=/_\n";
+    cout << "   /  _  \\\n";
+    cout << "  //|/.\\|\\\\\n";
+    cout << " ||  \\ /  ||\n";
+    cout << "============\n";
+    cout << "|          |\n";
+    cout << "|          |\n";
+    cout << "|          |\n";
+
+    return 0;
+}
