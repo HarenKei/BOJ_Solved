2e03c885221d8af01e26638ad3024ca41dc1ed75
Seungmin Jeong <seungmin1266@gmail.com>
2022-10-10 12:07:59 +0900

    Solved 17295 / cpp / Bronze V

---
+++ Bronze/17295.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string str;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> str;
+
+    cout << "Avengers: Endgame\n";
+
+    return 0;
+}
