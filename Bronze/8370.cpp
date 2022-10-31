2b8315ec740861c37e9232218bd60025fd823adc
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-10-31 23:50:49 +0900

    Solved 8370 / cpp / Bronze V

---
+++ Bronze/8370.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n1, k1, n2, k2;
+
+    cin >> n1 >> k1 >> n2 >> k2;
+
+    cout << (n1 * k1) + (n2 * k2) << '\n';
+    
+    return 0;
+}
