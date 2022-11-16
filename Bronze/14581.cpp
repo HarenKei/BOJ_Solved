c03f93dbeab56386971a90caad23719076708a18
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-11-16 22:29:45 +0900

    Solved 14581 / cpp / Bronze V

---
+++ Bronze/14581.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string hID;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> hID;
+
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << hID << "::fan:\n";
+    cout << ":fan::fan::fan:\n";
+    
+    return 0;
+}
