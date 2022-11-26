ca8bdbf407b36d42534890b6e2eb78e8d1e6aeae
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-11-26 23:23:55 +0900

    Solved 23235 / cpp / Bronze V

---
+++ Bronze/23235.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int caseNum = 1;
+
+    while(1){
+        int n;
+        cin >> n;
+
+        if(n == 0) return 0;
+
+        for(int i = 0; i < n; i++){
+            int input;
+            cin >> input;
+        }
+
+        cout << "Case " << caseNum <<": Sorting... done!\n";
+        caseNum++;
+    }
+
+
+    return 0;
+}
