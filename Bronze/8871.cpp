ec549274d37afb4902a2bf97c830dc95f87c5ce5
Jeong Seung Min <harenkei@email.daelim.ac.kr>
2022-12-10 21:58:18 +0900

    Solved 8871 / cpp / Bronze V

---
+++ Bronze/8871.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    
+    cin >> n;
+    n++;
+
+    cout << n * 2 << " " << n * 3 << '\n';
+
+    return 0;
+}
