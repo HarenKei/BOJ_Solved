26e916a4a1febfa7fc5137a6079fcfa30bd31169
HarenKei <seungmin1266@gmail.com>
2023-01-10 00:59:36 +0900

    Solved 15680 / cpp / Bronze IV

---
+++ Bronze/15680.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    if(n == 0){
+        cout << "YONSEI\n";
+    } else if(n == 1){
+        cout << "Leading the Way to the Future\n";
+    }
+
+    return 0;
+}
