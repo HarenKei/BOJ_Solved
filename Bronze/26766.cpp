be303e204c7937f24ba582427ea300faa17a5df5
HarenKei <seungmin1266@gmail.com>
2023-01-13 22:39:48 +0900

    Solved 26766 / cpp/ Bronze V

---
+++ Bronze/26766.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cout << " @@@   @@@ \n";
+        cout << "@   @ @   @\n";
+        cout << "@    @    @\n";
+        cout << "@         @\n";
+        cout << " @       @ \n";
+        cout << "  @     @  \n";
+        cout << "   @   @   \n";
+        cout << "    @ @    \n";
+        cout << "     @     \n";
+    }
+
+
+
+    return 0;
+}
