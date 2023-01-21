7867b892519571a6be85a7093ec2e7cc341d44eb
HarenKei <seungmin1266@gmail.com>
2023-01-21 23:44:11 +0900

    Solved 15963 / cpp / Bronze IV

---
+++ Bronze/15963.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long n, m;
+
+    cin >> n >> m;
+
+    if(n != m){
+        cout << 0 << "\n";
+    } else {
+        cout << 1 << "\n";
+    }
+    return 0;
+}
