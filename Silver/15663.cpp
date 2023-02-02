bda07dd177c0d91915c7dbae8ab2a6f4e32bc61b
HarenKei <seungmin1266@gmail.com>
2023-02-02 18:05:35 +0900

    Solved 15663 / cpp / Silver II

---
+++ Silver/15663.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+vector<int> v;
+
+set<vector<int> > s;
+bool visited[9];
+int arr[9];
+
+void DFS(int dep){
+    if(dep == m){
+        vector<int> tmp;
+        for(int i = 0; i < m; i++){
+            tmp.push_back(arr[i]);
+        }
+        s.insert(tmp);
+        tmp.clear();
+    } else {
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                arr[dep] = v[i];
+                DFS(dep + 1);
+                visited[i] = false;
+            } 
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    
+    for(int i = 0; i < n; i++){
+        int input;
+        cin >> input;
+        v.push_back(input);
+    }
+
+    sort(v.begin(), v.end());
+
+    DFS(0);
+
+    for(auto i : s){
+        for(auto j : i){
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
