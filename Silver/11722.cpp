1f08251aaf135ea53764cd3ce534d4ffd72d38d2
HarenKei <seungmin1266@gmail.com>
2023-02-15 22:34:23 +0900

    Solved 11722 / cpp / Silver II

--- Silver/11722.cpp
+++ Silver/11722.cpp
@@ -3,35 +3,33 @@
 
 using namespace std;
 
+int n;
+int arr[MAX];
+int dp[MAX];
+int maxNum;
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n;
-    vector<int> v;
-    int dp[MAX];
-    int result;
-
     cin >> n;
 
-    for(int i = 0; i < n; i++){
-        int input;
-        cin >> input;
-        v.push_back(input);
+    for(int i = 1; i <= n; i++){
+        cin >> arr[i];
         dp[i] = 1;
     }
         
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i <= n; i++){
         for (int j = 0; j < i; j++){
-            if(v[j] < v[i]){
+            if(arr[j] > arr[i]){ 
                 dp[i] = max(dp[i], dp[j] + 1);
             }
         } 
-        result = max(result, dp[i]);
+        maxNum = max(maxNum, dp[i]);
     }
 
-    cout << result << "\n";
+    cout << maxNum;
 
     return 0;
 }
