677ac58f8baf0f8d2f048cbf518040f4887aa36d
HarenKei <seungmin1266@gmail.com>
2023-02-16 22:53:29 +0900

    Solved 24444 / cpp / Silver II

---
+++ Silver/24444.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define MAX 100001
+
+using namespace std;
+
+queue<int> q;
+bool visited[MAX];
+vector<int> node[MAX];
+int result[MAX];
+int cnt = 0;
+
+void BFS(int start){
+    visited[start] = true;
+    q.push(start);
+    cnt++;
+    result[start] = cnt;
+
+    while(!q.empty()){
+        int cur = q.front();
+        q.pop();
+
+        for(int i = 0; i < node[cur].size(); i++){
+            int temp = node[cur][i];
+            if(!visited[temp]){
+                cnt++;
+                result[temp] = cnt;
+                visited[temp] = true;
+                q.push(temp);
+                
+            }
+        }
+    }
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m, r;
+    cin >> n >> m >> r;
+
+    for(int i = 0; i < m; i++){
+        int u = 0, v = 0;
+        cin >> u >> v;
+        node[u].push_back(v);
+        node[v].push_back(u);
+        //양방향 간선 구현
+    }
+
+    for(int i = 0; i < n; i++){
+        sort(node[i].begin(), node[i].end());
+        //오름차순 탐색을 위한 정렬.
+    }
+
+    BFS(r);
+
+    for(int i = 0; i < n; i++){
+        cout << result[i] << "\n";
+    }
+
+    return 0;
+}
