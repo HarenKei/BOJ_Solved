8772ffcba06efd905f157eca3b728e62c9294308
HarenKei <seungmin1266@gmail.com>
2023-02-18 00:36:58 +0900

    Solved 27327 / cpp / Bronze V

---
+++ Bronze/27327.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define MAX 100001
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int x;
+    cin >> x;
+
+    cout << x * 24 << "\n";
+
+
+    return 0;
+}
