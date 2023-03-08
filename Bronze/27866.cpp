6b92b1c7a08b5469ece323471bad4d050cfe9e4b
HarenKei <seungmin1266@gmail.com>
2023-03-08 18:51:55 +0900

    Solved 27866 / cpp / Bronze V

---
+++ Bronze/27866.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s;
+    int i;
+
+    cin >> s;
+    cin >> i;
+
+    cout << s[i - 1] << "\n";
+
+
+
+
+
+
+
+
+    
+    return 0;
+}
