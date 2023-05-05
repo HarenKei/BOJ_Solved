d8c139e407af332b8b338e6a2cc3daceac347463
HarenKei <seungmin1266@gmail.com>
2023-05-05 23:51:52 +0900

    Solved 6778 / cpp / Bronze IV

---
+++ Bronze/6778.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int eyes, antenna;
+    cin >> antenna >> eyes;
+
+    if(antenna >= 3 && eyes <= 4) {
+        cout << "TroyMartian\n";
+    }
+
+    if(antenna <= 6 && eyes >= 2) {
+        cout << "VladSaturnian\n";
+    }
+
+    if(antenna <= 2 && eyes <= 3) {
+        cout << "GraemeMercurian\n";
+    }
+
+
+
+    
+
+    return 0;
+}
