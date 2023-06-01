dbce37b0e19af2ff7507dc864a35a08152494ff9
HarenKei <seungmin1266@gmail.com>
2023-06-01 17:14:54 +0900

    Solved 1918 / cpp / Gold V

---
+++ Gold/1918.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    string s;
+    stack<char> st;
+
+    cin >> s;
+
+    for(int i = 0; i < s.length(); i++) {
+        if(s[i] >= 'A' && s[i] <= 'Z') {
+            cout << s[i];
+        } else {
+            if(s[i] == '('){
+                st.push(s[i]);
+            } else if(s[i] == ')') {
+                while(!st.empty() && st.top() != '(') {
+                    cout << st.top();
+                    st.pop();
+                }
+                st.pop();
+            } else if(s[i] =='+' || s[i] == '-') {
+                while(!st.empty() && st.top() != '(') {
+                    cout << st.top();
+                    st.pop();
+                }
+                st.push(s[i]);
+            } else if(s[i] == '*' || s[i] == '/') {
+                while(!st.empty() && (st.top() == '*' || st.top() == '/')) {
+                    cout << st.top();
+                    st.pop();
+                }
+                st.push(s[i]);
+            }
+        }
+
+
+    }
+    while(!st.empty()) {
+        cout << st.top();
+        st.pop();
+    }
+
+
+
+    return 0;
+}
