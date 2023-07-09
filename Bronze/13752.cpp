3467d932d7623dfb9217a40c05a3ba6a43d8d42f
HarenKei <seungmin1266@gmail.com>
2023-07-09 23:25:55 +0900

    Solved 13752 / cpp / Bronze IV:

---
+++ Bronze/13752.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    while(n--) {
+        int k;
+        cin >> k;
+
+        for(int i = 0; i < k; i++) {
+            cout << "=";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
