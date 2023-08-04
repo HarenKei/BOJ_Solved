9a60987fcf6b4115cb8440f91f23bf59199d929f
HarenKei <seungmin1266@gmail.com>
2023-08-04 16:50:53 +0900

    Solved 9656 / cpp / Silver IV

---
+++ Silver/9656.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    if(n % 2 == 0) {
+        cout << "SK\n";
+    } else {
+        cout << "CY\n";
+    }
+
+    return 0;
+}
