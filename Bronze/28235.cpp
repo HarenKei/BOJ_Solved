fa3624bf4cb389ccb3b8d014fcd19d8a79372ec1
HarenKei <seungmin1266@gmail.com>
2023-08-13 23:10:47 +0900

    Solved 28235 / cpp / Bronze V

---
+++ Bronze/28235.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    string guho;
+    cin >> guho;
+
+    if(guho == "SONGDO") {
+        cout << "HIGHSCHOOL\n";
+    } else if(guho == "CODE") {
+        cout << "MASTER\n";
+    } else if(guho == "2023") {
+        cout << "0611\n";
+    } else if(guho == "ALGORITHM") {
+        cout << "CONTEST\n";
+    }
+
+
+    return 0;
+}
