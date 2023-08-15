04f9d51875909fe2570d7f12a51397daaed319d3
HarenKei <seungmin1266@gmail.com>
2023-08-15 23:50:11 +0900

    Solved 14924 / cpp / Bronze IV

---
+++ Bronze/14924.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int s, t, d;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> s >> t >> d;
+    cout << d / (2 * s) * t << "\n";
+    
+    return 0;
+}
