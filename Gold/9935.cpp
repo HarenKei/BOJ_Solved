a0daec75ec67979c3d16c121ef0f2c34f89a12f2
HarenKei <seungmin1266@gmail.com>
2023-09-13 23:38:37 +0900

    Solved 9935 / cpp / Gold IV

---
+++ Gold/9935.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string target;
+string bomb;
+string ans = "";
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> target >> bomb;
+
+    for(int i = 0; i < target.length(); i++) {
+        ans += target[i];
+
+        if(ans.back() == bomb.back()) {
+            if(ans.length() < bomb.length()) continue;
+
+            string tmp;
+            for(int j = bomb.size() - 1; j >= 0; j--) {
+                if(ans.back() != bomb[j]) {
+                    reverse(tmp.begin(), tmp.end());
+                    ans += tmp;
+                    tmp = "";
+                    break;
+                } else {
+                    tmp += ans.back();
+                    ans.pop_back();
+                }
+            }
+        }
+    }
+
+    if(ans.empty()) {
+        cout << "FRULA\n";
+    } else {
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
