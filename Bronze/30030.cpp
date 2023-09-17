0bb420aab58e406d4e23963f7fdebd75764d72c0
HarenKei <seungmin1266@gmail.com>
2023-09-17 22:51:02 +0900

    Solved 30030 / cpp / Bronze V

---
+++ Bronze/30030.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int b;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> b;
+
+    cout << b / 1.1 << "\n";
+
+    return 0;
+}
