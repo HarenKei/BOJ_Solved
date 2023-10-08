6d6503448408df5863bfbd093daaa3141f6667d3
HarenKei <seungmin1266@gmail.com>
2023-10-08 23:48:01 +0900

    Solved 30328 / cpp / Bronze V

---
+++ Bronze/30328.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int input;
+    cin >> input;
+
+    cout << input * 4000 << "\n";
+
+    return 0;
+}
