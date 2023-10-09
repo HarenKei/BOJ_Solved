562d37ee9f9091f099e4b80ff3deaee4ad913775
HarenKei <seungmin1266@gmail.com>
2023-10-09 23:44:42 +0900

    Solved 27389 / cpp / Bronze V

---
+++ Bronze/27389.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    float n;
+    cin >> n;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << n / 4 << "\n";
+
+    return 0;
+}
