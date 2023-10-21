217c3b7419679fc4f54eb0c88ac8f575c8eef6ec
HarenKei <seungmin1266@gmail.com>
2023-10-22 01:06:02 +0900

    Solved 2393 / cpp / Bronze V

---
+++ Bronze/2393.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "  ___  ___  ___" << endl;
+    cout << "  | |__| |__| |" << endl;
+    cout << "  |           |" << endl;
+    cout << "   \\_________/" << endl;
+    cout << "    \\_______/" << endl;
+    cout << "     |     |" << endl;
+    cout << "     |     |" << endl;
+    cout << "     |     |" << endl;
+    cout << "     |     |" << endl;
+    cout << "     |_____|" << endl;
+    cout << "  __/       \\__" << endl;
+    cout << " /             \\" << endl;
+    cout << "/_______________\\" << endl;
+
+
+
+
+
+
+    return 0;
+}
