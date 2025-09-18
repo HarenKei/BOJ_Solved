8cedf233682ad86cf273cb8cb554535196b41c2c
HarenKei <seungmin1266@gmail.com>
2025-09-19 01:59:06 +0900

    Solved 32384 / cpp / Bronze V

---
+++ Bronze/32384.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    string str = "LoveisKoreaUniversity";
+    int n;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cout << str << " ";
+    }
+
+    cout << "\n";
+    
+    return 0;
+}
