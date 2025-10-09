63a87641927ee28470a7277962b7a4506045960d
HarenKei <seungmin1266@gmail.com>
2025-10-10 01:35:48 +0900

    Solved 31606 / cpp / Bronze V

---
+++ Bronze/31606.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int ringo, mikan;
+    const int banana = 3;
+
+    cin >> ringo >> mikan;
+
+    cout << (ringo + mikan + banana) << "\n";
+}
